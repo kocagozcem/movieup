ce439c34b3ecfa9b54961be1a788abbe05543d5a
Cem Kocagöz <kocagozcem@gmail.com>
2020-10-26 03:48:49 +0300

    Added: Popular movies carousel to home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import Head from "next/head";
-import CarouselItem from "../components/carousel/carousel-item/carousel-item";
+import CarouselTitle from "../components/carousel/carousel-title/carousel-title";
 import Header from "../components/header/header";
 import Hero from "../components/hero/hero";
 import MovieFilter from "../components/movie-filter/movie-filter";
 import styles from "../styles/Home.module.scss";
-import { ScrollingCarousel } from "@trendyol-js/react-carousel";
 import Carousel from "../components/carousel/carousel";
+import { useEffect } from "react";
+
+const popularMovies = [
+  {
+    imdbID: "tt0120737",
+  },
+  {
+    imdbID: "tt0068646",
+  },
+  {
+    imdbID: "tt0108052",
+  },
+  {
+    imdbID: "tt0108052",
+  },
+];
 
 export default function Home() {
+  useEffect(async () => {}, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>MovieUP | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -19,9 +36,10 @@ export default function Home() {
         <Header />
         <Hero />
         <div className={styles.filterContainer}>
-          <MovieFilter />
+          <MovieFilter onHero={true} />
         </div>
-        <Carousel />
+        <CarouselTitle title="Popular Movies" />
+        <Carousel movies={popularMovies} />
       </main>
     </div>
   );
