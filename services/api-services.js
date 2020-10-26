ac85a2f21515de9956e2843cd2168f15894a98bf
Cem Kocagöz <kocagozcem@gmail.com>
2020-10-26 03:40:00 +0300

    Added: Omdb api services

---
+++ services/api-services.js
@@ -0,0 +1,25 @@
+const apiKey = "c7b766bd"; // api key to use omdbapi
+
+// get request link
+const getDataUrl = (searchParams) => {
+  const url = `http://www.omdbapi.com/?apiKey=${apiKey}&${searchParams}&type=movie`;
+  return url;
+};
+
+// search movies by title
+export const searchMovieByTitle = async (title, page = 1) => {
+  const url = getDataUrl(`s=${title}&page=${page}`);
+  return await fetch(url).then((response) => response.json());
+};
+
+// get movie by given id
+export const searchMovieByID = async (id, page = 1) => {
+  const url = getDataUrl(`i=${id}&page=${page}`);
+  return await fetch(url).then((response) => response.json());
+};
+
+// get movie by given title
+export const searchMovieByImdbTitle = async (id, page = 1) => {
+  const url = getDataUrl(`t=${id}&page=${page}`);
+  return await fetch(url).then((response) => response.json());
+};
