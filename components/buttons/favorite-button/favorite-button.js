6614027761d295f79d5450742d3a4a2a5784733c
Cem Kocagöz <kocagozcem@gmail.com>
2020-10-26 03:43:34 +0300

    Added: Favorite button adds movie to favs

--- components/buttons/favorite-button/favorite-button.js
+++ components/buttons/favorite-button/favorite-button.js
@@ -3,13 +3,29 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./favorite-button.module.scss";
 
-export default function FavoriteButton({ isAdded = false }) {
+export default function FavoriteButton({ isAdded = false, movie }) {
   const [active, setActive] = useState(isAdded);
+
+  function addToFavorites() {
+    // get favorite movies from local storage
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    if (active) {
+      // remove movie from favorites
+      favorites = favorites.filter((fav) => fav["imdbID"] !== movie["imdbID"]);
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      setActive(!active);
+    } else {
+      // add movie to favorites
+      favorites = [...favorites, movie];
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      setActive(!active);
+    }
+  }
 
   return (
     <button
       className={active ? styles.containerActive : styles.container}
-      onClick={() => setActive(!active)}
+      onClick={() => addToFavorites()}
     >
       <span className={styles.icon}>
         <FontAwesomeIcon icon={faHeart} size="2x" className={styles.icon} />
