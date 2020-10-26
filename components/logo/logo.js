5029e69e12dd90f592efbc2514e25d5e370b71c5
Cem Kocagöz <kocagozcem@gmail.com>
2020-10-26 03:44:47 +0300

    Added: Navigate to home page by clicking logo

--- components/logo/logo.js
+++ components/logo/logo.js
@@ -1,9 +1,17 @@
+import { useRouter } from "next/router";
 import styles from "./logo.module.scss";
 
 export default function Logo() {
+  const router = useRouter();
+
+  // navigate to home page when user pressed to logo
+  function navigateToHome() {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
-      <span>
+      <span onClick={() => navigateToHome()}>
         Movie<span className={styles.up}>UP</span>
       </span>
     </div>
